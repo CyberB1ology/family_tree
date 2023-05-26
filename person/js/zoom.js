c1ca7bcb9119a1655942710a3c6d06b6a94ef36c
Anj <129781550+CyberB1ology@users.noreply.github.com>
2023-05-26 17:13:16 +0300

    Add files via upload

---
+++ person/js/zoom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#ex2').zoom({ on:'click' });
+	$('#ex3').zoom({ on:'click' });
+	$('#ex4').zoom({ on:'click' });	
+	$('#ex5').zoom({ on:'click' });				 
+	$('#ex6').zoom({ on:'click' });	
+	$('#ex7').zoom({ on:'click' });	
+	$('#ex8').zoom({ on:'click' });	
+	$('#ex9').zoom({ on:'click' });	
+	$('#ex10').zoom({ on:'click' });	
+	$('#ex11').zoom({ on:'click' });	
+	$('#ex12').zoom({ on:'click' });	
+	$('#ex13').zoom({ on:'click' });	
+});
